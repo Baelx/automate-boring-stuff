bed6e459eab085dd71bd5627edd2d108b06d7ff8
Alex Wintschel <awintschel@gmail.com>
2017-12-08 11:01:52 -0500

    Finished guessing game. Must add error handling for non-int input

---
+++ functions/guess-number.py
@@ -0,0 +1,23 @@
+# guess the number game
+
+import random
+
+secret_number = random.randint(1,20)
+print("Guess a number between 1 and 20")
+
+# Ask player to guess six times
+for guesses_taken in range(1,7):
+	print("Guess it")
+	guess = int(input())
+
+	if guess < secret_number:
+		print("Ha Ha, too low.")
+	elif guess > secret_number:
+		print("Ha Ha, too high.")
+	else:
+		break # This means they got it.
+
+if guess == secret_number:
+	print("Fine you win. It took you " + str(guesses_taken) + " guesses." )
+else:
+	print("Forget it, I'm not going to sit around here and let you be wrong all day")
