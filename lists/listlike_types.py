bc0e35084374a00a77246dbf4f525cba19be9ed3
Alex Wintschel <awintschel@gmail.com>
2017-12-15 10:59:43 -0500

    playing around with string methods

---
+++ lists/listlike_types.py
@@ -0,0 +1,15 @@
+name = 'Zalex'
+print(name[0])
+
+print("Slice up your life, " + name[1:])
+
+print("Can you get alex from Zalex?")
+
+print('alex' in name)
+
+print("Can you not get bob from Zalex?")
+
+print('bob' not in name)
+
+for i in name:
+	print(i)
